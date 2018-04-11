840cd08650aecddf859b0e6fb0bb8d11bcd41c65
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-04-11 19:21:48 +0100

    Remove .cache from GitHub

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/mib/GitHub/blog/src/layouts/index.js"
-  import data from "/Users/mib/GitHub/blog/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
